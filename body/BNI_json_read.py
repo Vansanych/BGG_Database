d8b58527930b32bb7de1869f3f75db7797c402b4
Иван <e741@mail.ru>
2022-04-21 22:36:54 +0300

    менял адреса файлов для чтения

--- body/BNI_json_read.py
+++ body/BNI_json_read.py
@@ -1,9 +1,8 @@
 import json
 
-with open('../files/BNI.json') as file:
+with open('../files/BNI_21_04_22.json') as file:
     last_posts = json.load(file)
 
 post_list = last_posts["response"]["items"]
-print(post_list)
 
 

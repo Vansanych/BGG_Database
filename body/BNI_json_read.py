b11311bb4c6d396a9f082592aa7a750f02d83b6a
Иван <e741@mail.ru>
2022-04-26 11:51:45 +0300

    изменил дату читаемого файла на 25.04.2022

--- body/BNI_json_read.py
+++ body/BNI_json_read.py
@@ -1,6 +1,6 @@
 import json
 
-with open('../files/BNI_21_04_22.json') as file:
+with open('../files/BNI_25_04_22.json') as file:
     last_posts = json.load(file)
 
 post_list = last_posts["response"]["items"]

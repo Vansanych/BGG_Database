4dfad7bb85fcdbec6831f3799c6aaa5502bab691
Иван <e741@mail.ru>
2022-10-06 14:41:39 +0300

    [20, 45]

--- body/filter_2.py
+++ body/filter_2.py
@@ -1,28 +1,27 @@
 """
 filter_1 возвращает список первых слов из строчек поста в качестве имен игр
-BNI_19_04_22[20, 23]
+BNI_19_04_22[20, 44]
 """
 from sorting_posts import sell_posts_list
 import re
 
 
-def filter_1(strings):
+def filter_2(strings):
     for string in strings:
         words = string.split()
-        # print(words)
         if len(words) > 1:
             if re.search(r'[0-9]', words[0]):
                 words.pop(0)
-            if re.search(r'\W', words[0]):
-                words[0] = words[0][:-1]
+            words[0] = re.sub(r'\W', '', words[0])
             names_list.append(words[0])
     return names_list
 
 
-post_number = 23
+post_number = 45
 post = sell_posts_list[post_number]
 strings = post.split('\n')
 names_list = []
+games = {}
 
-# print(post)
-print(filter_1(strings))
+print(post)
+print(filter_2(strings))

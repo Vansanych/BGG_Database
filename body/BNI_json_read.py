558b2185e5a84c77adc3709d22056635e4e14934
Иван <e741@mail.ru>
2022-10-09 17:56:25 +0300

    if __name__ == '__main__': added

--- body/BNI_json_read.py
+++ body/BNI_json_read.py
@@ -4,5 +4,6 @@ with open('../files/BNI_19_04_22.json') as file:
     last_posts = json.load(file)
 
 post_list = last_posts["response"]["items"]
-
-
+if __name__ == '__main__':
+    print('id:', post_list[0]['id'], '\n')
+    print(post_list[0]['text'])

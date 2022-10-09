16be51ad01c8ca9282e7ee31fb01f39482215e80
Иван <e741@mail.ru>
2022-10-09 19:42:49 +0300

    english names added

--- body/search_names.py
+++ body/search_names.py
@@ -6,12 +6,15 @@ from filter_2 import names
 import json
 
 with open(fr'../TopList/first_name_rus.json') as file:
-    names_list_tesera = json.load(file)
+    names_list_tesera_rus = json.load(file)
+
+with open(fr'../TopList/first_name_eng.json') as file:
+    names_list_tesera_eng = json.load(file)
 
 names_list = []
 trash_list = []
 for name in names:
-    if name in names_list_tesera:
+    if name in names_list_tesera_rus or name in names_list_tesera_eng:
         names_list.append(name)
     else:
         trash_list.append(name)

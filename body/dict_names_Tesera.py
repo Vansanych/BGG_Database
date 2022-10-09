690dddc2315a8f06235c2dce67d4c7b2e82937f9
Иван <e741@mail.ru>
2022-10-10 00:07:47 +0300

    list changed to dictionary

--- body/dict_names_Tesera.py
+++ body/dict_names_Tesera.py
@@ -8,37 +8,36 @@ JSON-файлы формируются в модуле TeseraTop1000.
 import json
 import re
 
-first_name_rus = []
-first_name_eng = []
+dict_name_rus = {}
+dict_name_eng = []
 for name in range(60):
     with open(fr'../TeseraTop1000/{name}.json') as file:
         g = json.load(file)
         for r in range(len(g)):
-            f_n_r = g[r]['title'].split()[0]
-            f_n_r = re.sub(r'\W', '', f_n_r)
-            first_name_rus.append(f_n_r)
-            # print(g[r]['title'])
+            f_n_r = g[r]['title'].split()
+            f_n_r[0] = re.sub(r'\W', '', f_n_r[0])
+            dict_name_rus[g[r]['title']] = len(f_n_r)
             try:
-                f_n_e = g[r]['title2'].split()[0]
-                f_n_e = re.sub(r'\W', '', f_n_e)
-                first_name_eng.append(f_n_e)
+                f_n_e = g[r]['title2'].split()
+                f_n_e[0] = re.sub(r'\W', '', f_n_e[0])
+                dict_name_eng[f_n_e[0]] = len(f_n_e)
             except Exception as ex:
                 print(g[r]['title'], "have no title2")
 
             # print(g[r].keys())
     # print(g['id'], g['testerId'], g['title'], g['title2'])
 
-with open(fr'../TopList/first_name_rus.json', 'w+') as file:
-    file.write(json.dumps(first_name_rus, indent=4))
+with open(fr'../TopList/dict_name_rus.json', 'w+') as file:
+    file.write(json.dumps(dict_name_rus, indent=4))
 
-with open(fr'../TopList/first_name_eng.json', 'w+') as file:
-    file.write(json.dumps(first_name_eng, indent=4))
+with open(fr'../TopList/dict_name_eng.json', 'w+') as file:
+    file.write(json.dumps(dict_name_eng, indent=4))
 
 
-# with open(fr'../TopList/first_name_rus.json') as file:
-#     g = json.load(file)
+with open(fr'../TopList/dict_name_rus.json') as file:
+    g = json.load(file)
 # print([(x, y) for x, y in enumerate(g)])
-# print(g)
+print(g)
 
 # with open(fr'../TopList/first_name_eng.json') as file:
 #     g = json.load(file)

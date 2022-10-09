728baa093a91f64608724f3d80d5490c1e4f0a87
Иван <e741@mail.ru>
2022-10-09 17:57:14 +0300

    init

---
+++ body/exe.py
@@ -0,0 +1,5 @@
+import BNI_json_write
+
+BNI_json_write.json_file()
+
+print(1)
---
+++ body/schedule.py
@@ -0,0 +1,22 @@
+import schedule
+# import time
+# import BNI_json_write
+
+
+def my():
+    print(1)
+
+
+my()
+
+# def main():
+# #schedule.every().day.at("21:18:50").do(job)
+#     schedule.every(4).seconds.do(job)
+#
+#     while True:
+#         schedule.run_pending()
+#         #time.sleep(TeseraTop1000)
+#
+#
+# if __name__ == '__main__':
+#     main()

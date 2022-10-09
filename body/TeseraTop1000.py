893b84c16369331e805c10e952bb0d9c8a8d60be
Иван <e741@mail.ru>
2022-10-09 17:46:07 +0300

    exe

---
+++ body/TeseraTop1000.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+for name in range(60):
+    url = fr'https://api.tesera.ru/games?offset={name}&limit=15&sort=-ratingn10'
+    res = requests.get(url).json()
+    print(f'parsing {name} page is running')
+    with open(fr'../TeseraTop1000/{name}.json', 'w+') as file:
+        file.write(json.dumps(res, indent=4))

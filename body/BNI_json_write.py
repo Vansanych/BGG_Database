c2dcac41bf03a258b14ab675a0e5df5d1922c394
Иван <e741@mail.ru>
2022-10-09 17:56:02 +0300

    date added

--- body/BNI_json_write.py
+++ body/BNI_json_write.py
@@ -3,12 +3,19 @@
 """
 import requests
 import json
+import datetime
 
 token = "79ad5ce9a9ac8da0b5b301bee4f6195bb3350de41ad71a56a555f39cb1de21e5459676965216297336607"
 url = f"https://api.vk.com/method/wall.get?domain=baraholkanastolok&count=100&access_token={token}&v=5.131"
 req = requests.get(url)
 t = req.json()
 post_list = t["response"]["items"]
+date = datetime.date.today()
 
-with open('../files/BNI_26_04_22.json', 'w') as f:
-    f.write(json.dumps(t))
+
+def json_file():
+    with open(f'C:/Users/Иван/Desktop/Иван/python/BG_database/files/BNI_{date}.json', 'w+') as f:
+        f.write(json.dumps(t))
+
+
+json_file()

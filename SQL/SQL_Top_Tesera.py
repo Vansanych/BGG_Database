5e4f299e56e176799e19e6e5c36bdf096615f5e9
Иван <e741@mail.ru>
2022-10-11 14:34:00 +0300

    keys dict added

--- SQL/SQL_Top_Tesera.py
+++ SQL/SQL_Top_Tesera.py
@@ -1,11 +1,25 @@
 import sqlite3
 import json
-# from logpas import x
+
 
 base = sqlite3.connect('Tesera_Top.db')
 cur = base.cursor()
 
-base.execute('CREATE TABLE IF NOT EXISTS {}(idBGG, title)'.format('data'))
+with open(fr'../TeseraTop1000/{0}.json') as file:
+    g = json.load(file)
+key_list = []
+for key in g[0].keys():
+    key_list.append(key)
+values_list = []
+for value in g[0].values():
+    values_list.append(value)
+print(', '.join([x for x in key_list]))
+
+base.execute('CREATE TABLE IF NOT EXISTS {}(id, teseraId, bggId, title, title2, title3, alias, descriptionShort, '
+             'description, modificationDateUtc, creationDateUtc, photoUrl, year, ratingUser, n10Rating, n20Rating, '
+             'bggRating, bggGeekRating, bggNumVotes, numVotes, playersMin, playersMax, playersMinRecommend, '
+             'playersMaxRecommend, playersAgeMin, timeToLearn, playtimeMin, playtimeMax, commentsTotal, '
+             'commentsTotalNew, isAddition)'.format('data'))
 base.commit()
 
 # cur.executemany('INSERT INTO data VALUES(?, ?)', (x))
@@ -15,5 +29,10 @@ for name in range(60):
     with open(fr'../TeseraTop1000/{name}.json') as file:
         g = json.load(file)
         for r in range(len(g)):
-            cur.execute('INSERT INTO data VALUES(?, ?)', (g[r]['bggId'], g[r]['title']))
+            values_list = []
+            for value in g[r].values():
+                values_list.append(value)
+            print(name, r)
+            cur.execute('INSERT INTO data VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,'
+                        ' ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', values_list)
             base.commit()

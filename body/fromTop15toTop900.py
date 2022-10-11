22798c0c042cdccee96ffebd81b39919a5b582ce
Иван <e741@mail.ru>
2022-10-11 14:34:10 +0300

    init

---
+++ body/fromTop15toTop900.py
@@ -0,0 +1,12 @@
+import json
+
+f = []
+for name in range(60):
+    with open(fr'../TeseraTop1000/{name}.json') as file:
+        g = json.load(file)
+        for r in range(len(g)):
+            f.append(g[r])
+
+
+with open(fr'../TopList/Top900.json', 'w+') as file:
+    file.write(json.dumps(f, indent=4))

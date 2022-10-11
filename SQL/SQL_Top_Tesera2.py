6ee6cabd67e19fab536901a34dbe1111866e4d87
Иван <e741@mail.ru>
2022-10-11 19:01:20 +0300

    comments added

--- SQL/SQL_Top_Tesera2.py
+++ SQL/SQL_Top_Tesera2.py
@@ -2,17 +2,18 @@ import sqlite3
 import json
 
 
-base = sqlite3.connect('Tesera_Top2.db')
+base = sqlite3.connect('Tesera_Top2.db')  # создание базы
 cur = base.cursor()
 
 with open(fr'../TopList/Top900_1.json') as file:
     games = json.load(file)
-str_key_list = ', '.join([x for x in games[0].keys()])
+str_key_list = ', '.join([x for x in games[0].keys()])  # создание строки с именами ключей для названия столбцов таблицы
 
-base.execute(f'CREATE TABLE IF NOT EXISTS data ({str_key_list})')
+base.execute(f'CREATE TABLE IF NOT EXISTS data ({str_key_list})')  # создание столбцов таблицы
 base.commit()
 
 for number, game in enumerate(games):
     print(number)
     cur.execute(f'INSERT INTO data VALUES({", ".join("?"*len(game))})', [value for value in game.values()])
+    # заполнение таблицы
     base.commit()

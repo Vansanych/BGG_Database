47787b7e0d345bd38fc8e61a3f04901eec38e221
Иван <e741@mail.ru>
2022-10-12 15:29:03 +0300

    searching by second names added

--- body/filter_3.py
+++ body/filter_3.py
@@ -7,21 +7,27 @@ import re
 
 
 def filter_2(strings):
+    names_list = []
     for string in strings:
         words = string.split()
         if len(words) > 1:
+            print(words)
             if re.search(r'[0-9]', words[0]):
                 words.pop(0)
-            words[0] = re.sub(r'\W', '', words[0])
-            names_list.append([words[0], words[1]])
+            try:
+                words[0] = re.sub(r'\W', '', words[0])
+                words[1] = re.sub(r'\W', '', words[1])
+                names_list.append([words[0], words[1]])
+            except Exception as ex:
+                print(ex, ' - after delete digits in words')
     return names_list
 
 
-post_number = 59
-post = sell_posts_list[post_number]
-strings = post.split('\n')
-names_list = []
+def get_strings(post_number):
+    post = sell_posts_list[post_number]
+    print(post)
+    return post.split('\n')
 
-print(post)
-names = filter_2(strings)
-print(names)
+
+# names = filter_2(get_strings(1))
+# print(names)

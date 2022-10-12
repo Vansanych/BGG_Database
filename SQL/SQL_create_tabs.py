8a80b3788c70257ca28be8bb3799cb9595b5bd5a
Иван <e741@mail.ru>
2022-10-12 11:16:02 +0300

    new base created

--- SQL/SQL_create_tabs.py
+++ SQL/SQL_create_tabs.py
@@ -1,13 +1,14 @@
 import sqlite3
 
 base = sqlite3.connect('Tesera_Top2.db')  # соединение с исходной базой
+base2 = sqlite3.connect('Games.db')  # соединение с новой базой
 cur = base.cursor()
 cur.execute('SELECT title FROM data')
 all_names = cur.fetchall()
 for game in all_names:
     alias = game[0].split()
     print(alias)
-    base.execute(f'CREATE TABLE IF NOT EXISTS {alias} (id PRIMARY KEY, name, price, date, town, text)')
+    base2.execute(f'CREATE TABLE IF NOT EXISTS {alias} (id PRIMARY KEY, name, price, date, town, text)')
     # создание столбцов таблицы
 base.commit()
 

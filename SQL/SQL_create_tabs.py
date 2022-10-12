16cd546e32831cfcbf41bed40b5047f722f29d6b
Иван <e741@mail.ru>
2022-10-12 14:40:31 +0300

    pretty tabs names

--- SQL/SQL_create_tabs.py
+++ SQL/SQL_create_tabs.py
@@ -1,15 +1,17 @@
 import sqlite3
 
 base = sqlite3.connect('Tesera_Top2.db')  # соединение с исходной базой
-base2 = sqlite3.connect('Games.db')  # соединение с новой базой
+base2 = sqlite3.connect('Games3.db')  # соединение с новой базой
 cur = base.cursor()
+cur2 = base2.cursor()
 cur.execute('SELECT title FROM data')
 all_names = cur.fetchall()
 for game in all_names:
-    alias = game[0].split()
+    alias = game[0].replace('"', '')
     print(alias)
-    base2.execute(f'CREATE TABLE IF NOT EXISTS {alias} (id PRIMARY KEY, name, price, date, town, text)')
+    base2.execute('CREATE TABLE IF NOT EXISTS "{}" (id PRIMARY KEY, name, price, date, town, text)'.format(alias))
     # создание столбцов таблицы
+    # cur2.execute(f'INSERT INTO {alias} VALUES({", ".join("?" * len(game))})', [value for value in game.values()])
 base.commit()
 
 

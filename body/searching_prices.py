23c66d51f8effee6ba057a4c76183c8ef1901da8
Иван <e741@mail.ru>
2022-10-14 14:16:15 +0300

    condition for one word in name added price < 100_000

--- body/searching_prices.py
+++ body/searching_prices.py
@@ -2,17 +2,24 @@ from sorting_posts import sell_posts_list
 from search_names_2 import names_list
 import re
 
-post = sell_posts_list[2]  # выбираем номер поста
+post = sell_posts_list[5]  # выбираем номер поста
 post_clear = re.sub("\W", " ", post)  # удаляет знаки препинания
 words_list = post_clear.split()  # разбивает пост на отдельные слова
 digits = re.findall('\d+', post_clear)  # находим цифры в посте
-prices = [price for price in digits if int(price) // 50]  # составляем список цен
+prices = [price for price in digits if int(price) // 50 and int(price) < 100_000]  # составляем список цен
 price_list = {}
 for name in names_list:
     for price in prices:
-        if words_list.index(price) > words_list.index(name[0]):
-            price_list[name[0] + ' ' + name[1]] = price  # присваиваем названию игры стоимость, ближайшую по тексту
-            break
+        if type(name) == list:
+            if words_list.index(price) > words_list.index(name[0]):
+                price_list[name[0] + ' ' + name[1]] = price  # присваиваем названию игры стоимость, ближайшую по тексту
+                break
+        else:
+            if words_list.index(price) > words_list.index(name):
+                price_list[name] = price  # присваиваем названию игры стоимость, ближайшую по тексту
+                break
 
 print(post)
+print(names_list)
+print(prices)
 print(price_list)

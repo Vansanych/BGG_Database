331e023d525b79e7cbf2d7f479f122f9d3cfe5f9
Иван <e741@mail.ru>
2022-10-29 22:42:59 +0300

    init

---
+++ body/hv.py
@@ -0,0 +1,12 @@
+import re
+
+
+post = '1500 3'
+price = '1500'
+post_clear = re.sub(f'{price}\S', f'{price}', post)  # убираем знаки после цены в посте
+print(post_clear)
+match = re.match('1500', post)
+print(match[0])
+
+h = [1, 4, 5]
+print(h.index(1, 0))

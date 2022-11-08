cbf77864a6bb414cc3f24f2d0ca9131bece9aee1
Иван <e741@mail.ru>
2022-11-08 22:58:40 +0300

    clearing code

--- body/searching_prices3.py
+++ body/searching_prices3.py
@@ -1,7 +1,6 @@
 """
 Модуль выполняет поиск цен.
 Затем объединяет в словарь с названием игры.
-Вариант, где первым шагом происходит удаление всех ненужных чисел из поста.
 """
 
 from sorting_posts import sell_posts_list
@@ -9,17 +8,14 @@ from search_names_2 import names_list
 import re
 
 
-post = sell_posts_list[16]  # выбираем номер поста
+post = sell_posts_list[20]  # выбираем номер поста
 print(post, '\n')
 post_clear = re.sub("\W", " ", post)  # удаляет знаки препинания
-digits = re.findall('\d+', post_clear)  # находим цифры в посте
-prices = [price for price in digits if int(price) // 50 and int(price) < 100_000]  # составляем список цен
-odd_digits = [digit for digit in digits if digit not in prices]
 price_list = {}
 words = post_clear.split()
 
 
-def searching_function(first_name, lst, price):
+def searching_function(first_name, lst):
     lst = lst[lst.index(first_name)::]  # поиск начинается с названия игры
     text = " ".join(lst)  # объединяем список из слов в текст
     print(text[:150:], '\n')
@@ -32,25 +28,11 @@ def searching_function(first_name, lst, price):
 
 for name in names_list:
     if type(name) == list:  # проверка количества слов в названии
-        for any_price in prices:
-            price = searching_function(name[0], words, any_price)
-            if price in prices:
-                price_list[name[0] + ' ' + name[1]] = price  # присваиваем названию игры стоимость, ближайшую по тексту
-                prices.pop(prices.index(price))
-                break
-            else:
-                post_clear = re.sub(f'{price}\S+', f'{price}', post_clear)
-                words = post_clear.split()
-                words.pop(words.index(price))
-                price = searching_function(name[0], words, any_price)
-                price_list[name[0] + ' ' + name[1]] = price  # присваиваем названию игры стоимость, ближайшую по тексту
+        price = searching_function(name[0], words)
+        price_list[name[0] + ' ' + name[1]] = price  # присваиваем названию игры стоимость, ближайшую по тексту
     else:
-        for any_price in prices:
-            price = searching_function(name, words, any_price)
-            while price not in prices:
-                words.pop(words.index(price))
-                price = searching_function(name, words, any_price)
-            price_list[name] = price  # присваиваем названию игры стоимость, ближайшую по тексту
+        price = searching_function(name, words)
+        price_list[name] = price  # присваиваем названию игры стоимость, ближайшую по тексту
 
 
 print(price_list)

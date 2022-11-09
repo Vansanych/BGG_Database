b04379ec6849227b2d89317d8be2341d55749005
Иван <e741@mail.ru>
2022-11-09 22:38:42 +0300

    function search_prices added

--- body/searching_prices3.py
+++ body/searching_prices3.py
@@ -4,16 +4,15 @@
 """
 
 from sorting_posts import sell_posts_list
-from search_names_2 import names_list
+from search_names_2 import search_names
 import re
 
 
-post = sell_posts_list[23]  # выбираем номер поста
-price_list = {}
-
-
 def searching_function(first_name, lst):
-    lst = lst[lst.index(first_name)::]  # поиск начинается с названия игры
+    try:
+        lst = lst[lst.index(first_name)::]  # поиск начинается с названия игры
+    except Exception as ex:
+        print(ex, first_name)
     try:
         match = re.search('\d{2,4}[0]', lst)[0]  # выполняем поиск цифр (3-5 значных с нолем в конце)
         return match
@@ -21,12 +20,16 @@ def searching_function(first_name, lst):
         return 0
 
 
-for name in names_list:
-    if type(name) == list:  # проверка количества слов в названии
-        price = searching_function(name[0], post)
-        price_list[name[0] + ' ' + name[1]] = price  # присваиваем названию игры стоимость, ближайшую по тексту
-    else:
-        price = searching_function(name, post)
-        price_list[name] = price  # присваиваем названию игры стоимость, ближайшую по тексту
+def search_prices(post_number):
+    names_list = search_names(post_number)[0]
+    post = sell_posts_list[post_number]  # выбираем номер поста
+    price_list = {}
+    for name in names_list:
+        if type(name) == list:  # проверка количества слов в названии
+            price = searching_function(name[0], post)
+            price_list[name[0] + ' ' + name[1]] = price  # присваиваем названию игры стоимость, ближайшую по тексту
+        else:
+            price = searching_function(name, post)
+            price_list[name] = price  # присваиваем названию игры стоимость, ближайшую по тексту
+    return price_list
 
-print(price_list)

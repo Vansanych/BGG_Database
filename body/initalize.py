af0e8e8c079ec08523c564c6444fe440edd15bc9
Иван <e741@mail.ru>
2022-11-13 16:54:48 +0300

    function dict_game_with_prices created

--- body/initalize.py
+++ body/initalize.py
@@ -6,5 +6,12 @@ from searching_prices3 import search_prices
 from sorting_posts import sell_posts_list
 
 
-for post in range(len(sell_posts_list)):
-    print(post, search_prices(post))
+def dict_games_with_prices():
+    games_dict = {}
+    for post in range(len(sell_posts_list)):
+        try:
+            if search_prices(post):
+                games_dict[post] = search_prices(post)
+        except Exception as ex:
+            print('initialize module', ex)
+    return games_dict

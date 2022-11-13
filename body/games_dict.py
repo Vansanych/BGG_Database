b596af5a3403c78a15a2a954b5d8749f5211f488
Иван <e741@mail.ru>
2022-11-13 17:15:58 +0300

    comments added

---
+++ body/games_dict.py
@@ -0,0 +1,20 @@
+"""
+Модуль создания словаря из словарей игр с ценами
+"""
+
+from searching_prices3 import search_prices  # импорт функции создания словаря с игрой и ее ценой
+from sorting_posts import sell_posts_list  # импорт списка постов
+
+
+def dict_games_with_prices():
+    games_dict = {}
+    for post in range(len(sell_posts_list)):
+        try:
+            if search_prices(post):  # проверка на наличие записей в словаре
+                games_dict[post] = search_prices(post)  # добавление словаря созданного из одного поста
+        except Exception as ex:
+            print('initialize module', ex)
+    return games_dict
+
+
+print(dict_games_with_prices())

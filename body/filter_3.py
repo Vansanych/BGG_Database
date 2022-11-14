ebda28a7909d111027d7f8d9b391c34e1a1818e9
Иван <e741@mail.ru>
2022-11-14 21:22:43 +0300

    body added to import exception_list added

--- body/filter_3.py
+++ body/filter_3.py
@@ -2,12 +2,13 @@
 filter_2 возвращает список двух первых слов из строчек поста в качестве имен игр
 BNI_19_04_22[20, 44]
 """
-from sorting_posts import sell_posts_list
+from body.sorting_posts import sell_posts_list
 import re
 
 
 def filter_2(strings):
     names_list = []
+    ex_list = []
     for string in strings:
         words = string.split()
         if len(words) > 1:
@@ -17,8 +18,10 @@ def filter_2(strings):
                 words[0] = re.sub(r'\W', '', words[0])
                 words[1] = re.sub(r'\W', '', words[1])
                 names_list.append([words[0], words[1]])
-            except Exception as ex:
-                print('filter_3', ex, ' - after delete digits in words')
+            except:
+                ex_list.append(string)
+    if ex_list:
+        print('filter_3', ex_list)
     return names_list
 
 

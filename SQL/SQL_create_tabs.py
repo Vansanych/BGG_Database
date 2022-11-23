d5a6bd4e23470ea16fe8219a1a73dd1a82810703
Иван <e741@mail.ru>
2022-11-23 22:01:16 +0300

    base url changed replace "" deleted

--- SQL/SQL_create_tabs.py
+++ SQL/SQL_create_tabs.py
@@ -4,14 +4,14 @@
 
 import sqlite3
 
-base = sqlite3.connect('Tesera_Top2.db')  # соединение с исходной базой
+base = sqlite3.connect('Tesera_Top3.db')  # соединение с исходной базой
 base2 = sqlite3.connect('../filters/Games4.db')  # соединение с новой базой
 cur = base.cursor()
 cur2 = base2.cursor()
 cur.execute('SELECT title FROM data')  # выбирает все ячейки из колонки title на листе data
 all_names = cur.fetchall()
 for game in all_names:
-    alias = game[0].replace('"', '')
+    alias = game[0]
     print(alias)
     base2.execute('CREATE TABLE IF NOT EXISTS "{}" (id PRIMARY KEY, name, price, date, town, text)'.format(alias))
     # создание столбцов таблицы

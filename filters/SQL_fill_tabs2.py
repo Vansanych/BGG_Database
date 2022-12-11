f094744984fd7e2b94a16e6948b3a271d6a69eab
Иван <e741@mail.ru>
2022-12-11 13:41:45 +0300

    rename dict_games to full_dict create tabs to fill added

--- filters/SQL_fill_tabs2.py
+++ filters/SQL_fill_tabs2.py
@@ -7,22 +7,23 @@ import sqlite3
 from body.searching_prices4 import full_dict
 
 base = sqlite3.connect('../SQL/Tesera_Top3.db')  # соединение с базой Top900
-base2 = sqlite3.connect('Games4.db')  # соединение с базой по продаже игр
+base2 = sqlite3.connect('Games5.db')  # соединение с базой по продаже игр
 cur = base.cursor()
 cur2 = base2.cursor()
 
-dict_games = full_dict
-for post_id in dict_games:
-    for game in dict_games[post_id][1]:
+for post_id in full_dict:
+    for game in full_dict[post_id][1]:
         cur.execute(f'SELECT title FROM data WHERE (title LIKE "%{game[0]}%") OR (title2 LIKE "%{game[0]}%")')
         # выборка из базы Тесера по названию игры
         name_tuple = cur.fetchone()  # находит первое совпадение
         if name_tuple:
             name = name_tuple[0]
-            date = dict_games[post_id][0]
-            price = dict_games[post_id][1][game][0]
-            text = dict_games[post_id][1][game][1]
+            date = full_dict[post_id][0]
+            price = full_dict[post_id][1][game][0]
+            text = full_dict[post_id][1][game][1]
             try:
+                cur2.execute('CREATE TABLE IF NOT EXISTS "{}" (id PRIMARY KEY, name, price, date, town, text)'
+                             .format(name))
                 cur2.execute(f'INSERT INTO "{name}" (id, name, price, date, text) '
                              f'VALUES ("{post_id}", "{name}", "{price}", "{date}", "{text}")')
             except Exception as ex:

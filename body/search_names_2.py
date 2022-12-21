8abed7abef2c0d24b36729f9f6c662739dd6b858
Иван <e741@mail.ru>
2022-12-21 20:22:58 +0300

    1. comparing len(list) in def search_words_in_tuple deleted 2. def search_names_from_dict2 made easier

--- body/search_names_2.py
+++ body/search_names_2.py
@@ -58,39 +58,37 @@ def search_matches_from_sql(column, words):
     Выдает кортеж из названий игр содержащих искомое слово"""
     base = sqlite3.connect('../filters/Tesera_Top5000.db')  # соединение с базой Top900
     cur = base.cursor()
-    cur.execute(f'SELECT "{column}" FROM data WHERE "{column}" LIKE "%{words[0]}%"')
+    cur.execute(f'SELECT "{column}" FROM data WHERE "{column}" LIKE "{words[0]}%"')
     # выборка из базы Тесера по названию игры
-    name_tuple = cur.fetchall()  # находит все совпадения
+    name_tuple = cur.fetchall()  # выдает все совпадения в виде списка list[(tuple,), (tuple,)...]
     return name_tuple
 
 
 def search_words_in_tuple(names_tuple, words):
-    names_list = []
-    new_words_list = names_list  # нужно для дальнейшего сравнения длин списков
-    for name_t in names_tuple:
-        name_t = str(name_t[0]).replace(':', ' ').replace('.', ' ')  # name_t[0] индекс нужен потому что это кортеж
-        names_list = [words[0]]
+    """Поиск совпадений между словами из строчки БНИ и словами кортежа из SQL"""
+    names_list = [words[0]]
+    for name_t in names_tuple[0].split():  # name_t[0] индекс нужен потому что это кортеж
+        name_t = str(name_t).replace(':', ' ').replace('.', ' ')
         for word in words[1:]:
-            if (word.capitalize() in name_t.split()) or (word.lower() in name_t.split()):
+            if (word.capitalize() in name_t) or (word.lower() in name_t):
                 # поиск слов с заглавной и прописной букв
                 names_list.append(word)
-        if len(names_list) > len(new_words_list):
-            new_words_list = names_list
-    return new_words_list
+    return names_list
 
 
-def search_names_from_dict2(names_list):
+def search_names_from_dict2_old(strings):
     """Функция составляет список слов названий из одного поста"""
     new_names_list = []
     columns_list = ['title', 'title2', 'title3']
-    for words in names_list:
+    for words in strings:  # words = []
         print(words)
-        names_tuple_list = []
+        name_tuples_list = []
         for column in columns_list:
-            names_tuple_list.append(search_matches_from_sql(column, words))
-        if names_tuple_list:
+            name_tuples_list.append(search_matches_from_sql(column, words))
+            # search_matches_from_sql(column, words) = [tuples], name_tuples_list = [list[tuples]]
+        if name_tuples_list:
             list_words_lists_title = []
-            for words_list_title in names_tuple_list:
+            for words_list_title in name_tuples_list:
                 name_words_list = search_words_in_tuple(words_list_title, words)
                 list_words_lists_title.append(name_words_list)
             if len(list_words_lists_title) > 1:
@@ -103,6 +101,27 @@ def search_names_from_dict2(names_list):
     return new_names_list
 
 
+def search_names_from_dict2(strings):
+    """Функция составляет список слов названий из одного поста"""
+    new_names_list = []
+    columns_list = ['title', 'title2', 'title3']
+    for words in strings[4:6]:  # words = []
+        name_words_list = []
+        print(words)
+        for column in columns_list:
+            names_tuples_from_column = search_matches_from_sql(column, words)
+            # search_matches_from_sql(column, words) = [tuples]
+            if names_tuples_from_column:
+                for name_tuple in names_tuples_from_column:
+                    name_words_list_new = search_words_in_tuple(name_tuple, words)
+                    if len(name_words_list_new) > len(name_words_list):
+                        name_words_list = name_words_list_new
+        print(name_words_list)
+        if name_words_list:
+            new_names_list.append(name_words_list)
+    return new_names_list
+
+
 def create_names_list(x):
     post = sell_posts_dict[x][1]
     post = re.sub(',', ' ', post)
@@ -110,17 +129,17 @@ def create_names_list(x):
     # for number, words in enumerate(strings):
     #     print(number, words)
     # print(strings)
-    names_list = filter_22(strings)
+    strings = filter_22(strings)  # list[lists]
     # print(names_list)
     # print(1, search_names_from_dict(names_list))
     # print(2, search_names_from_dict2(names_list))
-    return search_names_from_dict2(names_list)
+    return search_names_from_dict2(strings)
 
 
 full_dict = {}
-# for post_id in range(414134, 414135):
-for post_id in sell_posts_dict:
-    print(post_id, 'body.search_names_2')
+for post_id in range(414134, 414135):
+# for number, post_id in enumerate(sell_posts_dict):
+#     print(number+1, 'из', len(sell_posts_dict), post_id, 'body.search_names_2')
     words_in_name = create_names_list(post_id)
     if words_in_name:
         full_dict[post_id] = [sell_posts_dict[post_id][0], words_in_name, sell_posts_dict[post_id][1]]

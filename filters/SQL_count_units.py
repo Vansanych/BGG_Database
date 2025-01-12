b0000b01741cf21396496bbe98677071a05fe500
Иван <e741@mail.ru>
2025-01-12 19:54:52 +0300

    TOP 5000.

--- filters/SQL_count_units.py
+++ filters/SQL_count_units.py
@@ -4,16 +4,18 @@
 
 import sqlite3
 
-base = sqlite3.connect('../SQL/Tesera_Top3.db')  # соединение с базой Top900
-base2 = sqlite3.connect('Games4.db')  # соединение с базой по продаже игр
+base = sqlite3.connect('../filters/Tesera_Top5000_1.db')  # соединение с базой Top900
+base2 = sqlite3.connect('Games6_1.db')  # соединение с базой по продаже игр
 cur = base.cursor()
 cur2 = base2.cursor()
 cur2.execute("SELECT name FROM sqlite_master WHERE type='table'")  # выбор всех названий таблиц в базе
-base.execute('CREATE TABLE IF NOT EXISTS summary (id PRIMARY KEY, name, amount, med_price)')
+base.execute('DROP TABLE summary')
+base.execute('CREATE TABLE IF NOT EXISTS summary (id PRIMARY KEY, name, amount INTEGER, med_price)')
 # создание сводной таблицы со средней ценой и количеством записей
 
 all_names = cur2.fetchall()  # создание списка всех имен таблиц базы БНИ
-for name in all_names:
+for number, name in enumerate(all_names):
+    print(number)
     cur.execute(f'SELECT id FROM data WHERE title = "{name[0]}"')  # здесь нужно взять id из первой таблицы
     id1 = cur.fetchone()
     cur2.execute(f'SELECT * FROM "{name[0]}"')  # выбор всех записей из таблицы одной игры
@@ -21,8 +23,8 @@ for name in all_names:
     if count:
         amount = len(count)
         try:
+            # print(f'производится запись игры {name[0]}')
             cur.execute(f'INSERT INTO summary (id, name, amount) VALUES ("{id1[0]}", "{name[0]}", "{amount}")')
         except:
             pass
-
 base.commit()  # запись изменений
